9305c4d2fc5efc6fb241be86070018cb357c97a7
axolotl <andre.charles.lewin@gmail.com>
2017-07-24 23:48:11 +0200

    displayed the basic elements

--- app/index.js
+++ app/index.js
@@ -6,13 +6,51 @@ import './index.sass';
 
 function App() {
     return (
-		<div>
+		<div className="container">
 			<h1>WIP: Recipe box</h1>
-			<div className="well panel">
-
-
+			<div className="well panel panel-default">
+				<div className="panel-group">
+					<div className="panel panel-success">
+						<div className="panel-heading">
+							<h4 className="panel-title">
+								<button data-toggle="collapse" data-target="#demo">Friend</button>
+							</h4>
+						</div>
+						<div id="demo" className="panel-collapse">
+							<div className="panel-body">
+                                <ul className="list-group">
+                                    <li className="list-group-item">Trust</li>
+                                    <li className="list-group-item">Time</li>
+                                    <li className="list-group-item">Listening</li>
+                                    <li className="list-group-item">Helping</li>
+                                </ul>
+                                <button className="btn btn-danger">Delete</button>
+                                <button className="btn btn-default">Edit</button>
+							</div>
+						</div>
+					</div>
+                    <div className="panel panel-success">
+                        <div className="panel-heading">
+                            <h4 className="panel-title">
+                                <button data-toggle="collapse" data-target="#demo">Job</button>
+                            </h4>
+                        </div>
+                        <div id="demo" className="panel-collapse">
+                            <div className="panel-body">
+                                <ul className="list-group">
+                                    <li className="list-group-item">Long work</li>
+                                    <li className="list-group-item">Luck</li>
+                                    <li className="list-group-item">Family</li>
+                                    <li className="list-group-item">Skill</li>
+                                </ul>
+                                <button className="btn btn-danger">Delete</button>
+                                <button className="btn btn-default">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+				</div>
 			</div>
-			<button />
+			<button className="btn btn-primary">Add recipe</button>
 		</div>
     );
 }
